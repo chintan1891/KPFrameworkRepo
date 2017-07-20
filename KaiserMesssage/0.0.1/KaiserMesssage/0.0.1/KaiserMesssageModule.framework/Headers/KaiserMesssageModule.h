1f7ee2ee9df2510f0c1a2a5fd2b9c9bf9aa7e444
Chintan Prajapati <chintan.prajapati@infostretch.com>
2017-07-21 00:38:29 +0530

    Added Version 0.0.1 for KaiserMesssage

---
+++ KaiserMesssage/0.0.1/KaiserMesssage/0.0.1/KaiserMesssageModule.framework/Headers/KaiserMesssageModule.h
@@ -0,0 +1,19 @@
+//
+//  KaiserMesssageModule.h
+//  KaiserMesssageModule
+//
+//  Created by Chintan Prajapati on 13/07/17.
+//  Copyright © 2017 Infostretch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KaiserMesssageModule.
+FOUNDATION_EXPORT double KaiserMesssageModuleVersionNumber;
+
+//! Project version string for KaiserMesssageModule.
+FOUNDATION_EXPORT const unsigned char KaiserMesssageModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KaiserMesssageModule/PublicHeader.h>
+
+
