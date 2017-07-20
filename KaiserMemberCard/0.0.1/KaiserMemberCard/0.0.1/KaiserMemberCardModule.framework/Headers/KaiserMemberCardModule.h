b02083dfdfd5fc2a54dd6b2ceff13206afb49e66
Chintan Prajapati <chintan.prajapati@infostretch.com>
2017-07-21 00:37:47 +0530

    Added Version 0.0.1 for KaiserMemberCard

---
+++ KaiserMemberCard/0.0.1/KaiserMemberCard/0.0.1/KaiserMemberCardModule.framework/Headers/KaiserMemberCardModule.h
@@ -0,0 +1,19 @@
+//
+//  KaiserMemberCardModule.h
+//  KaiserMemberCardModule
+//
+//  Created by Chintan Prajapati on 13/07/17.
+//  Copyright © 2017 Infostretch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KaiserMemberCardModule.
+FOUNDATION_EXPORT double KaiserMemberCardModuleVersionNumber;
+
+//! Project version string for KaiserMemberCardModule.
+FOUNDATION_EXPORT const unsigned char KaiserMemberCardModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KaiserMemberCardModule/PublicHeader.h>
+
+
